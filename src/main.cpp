78389c1cd56aa3b63b6ded6d6e81c478c86b144f
Neemek <sindodeg@gmail.com>
2023-12-05 09:38:41 +0100

    better file read and debug tokens

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "radion/lexer.hpp"
 
 int main (int argc, char *argv[]) {
@@ -7,14 +8,17 @@ int main (int argc, char *argv[]) {
 		// Help or REPL
 	} else if (argc == 2) {
 		// Run file
-		ifstream f(argv[1]);
-		char src[1 << 12];
-		f >> src;
+		ifstream t(argv[1]);
+		std::stringstream buffer;
+		buffer << t.rdbuf();
+		
+		string src = buffer.str();
 
-		Lexer lexer;
-		vector<Token> tokens = lexer.lex(src);
+		vector<Token> tokens = Lexer::lex(src);
 
-		// test
+		for (int i=0; i < tokens.size(); i++) {
+			std::cout << tokens.at(i).lexeme() << std::endl;
+		}
 	}
 	
 	return 0;
