583d399a2910aae0de32e5bb47d260490fd13706
Neemek <sindodeg@gmail.com>
2023-12-30 17:25:35 +0100

    main file ready

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,32 @@
 #include <sstream>
 #include "radion/lexer.hpp"
 #include "radion/parser/parser.hpp"
+#include "radion/interpreter/interpreter.hpp"
+#include "radion/parser/nodes/block.hpp"
 
 int main (int argc, char *argv[]) {
 	if (argc <= 1) {
 		// Help or REPL
+		
 	} else if (argc == 2) {
 		// Run file
 		ifstream t(argv[1]);
 		std::stringstream buffer;
 		buffer << t.rdbuf();
-		
-		string src = buffer.str();
 
+		string src = buffer.str();
 		vector<Token> tokens = Lexer::lex(src);
 
 		Parser p(tokens, src);
+		BlockNode program = p.parse();
 
-		Node program = p.parse();
+		if (p.hadError) {
+			std::cout << "Program had errors, not evaluating" << std::endl;
+			return 1;
+		}
 
-		
+		Interpreter interpreter;
+		interpreter.evaluate(&program);
 	}
 	
 	return 0;
