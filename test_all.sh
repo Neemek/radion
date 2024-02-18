3747be9b9d19d97b260ed3126d968284cadb2ed3
Neemek <sindodeg@gmail.com>
2024-02-18 15:45:20 +0100

    fix test_all.sh script

--- test_all.sh
+++ test_all.sh
@@ -7,11 +7,13 @@ FAILED = false
 
 for file in ./examples/*.rn; do
     echo $'\e[32m[test_all.sh] Running file '$file$'\e[0m'
-    gtimeout 10s ./release/radion "$file"
+    gtimeout 5s ./release/radion "$file"
 
-    if [ 0 -eq $? ]; then
+    if [ 0 -ne $? ]; then
         FAILED=true
         echo $'\e[31m[test_all.sh] Program ran unsuccessfully\e[0m'
+    else
+        echo $'\e[32m[test_all.sh] Program ran successfully\e[0m'
     fi
 
     echo ""
