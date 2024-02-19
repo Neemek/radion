e2d89c81eb323719440fca15fc8d0d3c9b2bd838
Neemek <sindodeg@gmail.com>
2024-02-19 09:41:20 +0100

    remove make process from test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-cd release
-make
-cd ..
 
-FAILED = false
+FAILED=()
 
 for file in ./examples/*.rn; do
     echo $'\e[32m[test_all.sh] Running file '$file$'\e[0m'
     gtimeout 5s ./release/radion "$file"
 
     if [ 0 -ne $? ]; then
-        FAILED=true
+        FAILED+=($file)
         echo $'\e[31m[test_all.sh] Program ran unsuccessfully\e[0m'
     else
         echo $'\e[32m[test_all.sh] Program ran successfully\e[0m'
@@ -19,8 +16,12 @@ for file in ./examples/*.rn; do
     echo ""
 done
 
-if $FAILED; then
-    echo $'\e[31m[test_all.sh] Error running files\e[0m'
+if [ 0 -ne ${#FAILED[@]} ]; then
+    echo $'\e[31m[test_all.sh] Error running files:'
+    for file in ${FAILED[@]} ; do
+      echo "[test_all.sh] > $file"
+    done
+    echo $'\e[0m';
     exit 1
 else
     exit 0
